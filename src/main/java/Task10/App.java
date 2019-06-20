576a0a1fc97cd53dd9545cb2116c873e37210d0b
Vadym Golomoz <Vadym.Golomoz@issukraine.com>
2019-06-20 18:01:40 +0300

    Task10

---
+++ src/main/java/Task10/App.java
@@ -0,0 +1,9 @@
+package Task10;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        new Colony().makeColony(1000000);
+    }
+}
---
+++ src/main/java/Task10/Bacterium.java
@@ -0,0 +1,11 @@
+package Task10;
+
+public class Bacterium {
+
+    private String name;
+
+    public Bacterium(String name){
+        this.name = name;
+    }
+
+}
